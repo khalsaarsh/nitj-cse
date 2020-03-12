0d72e7eb8029063b59ccd8d3fd00e282e8fd8a86
arshdeep1535@gmail.com <arshdeep1535@gmail.com>
2020-03-12 13:40:42 +0530

    added dropDowns in navigation and tweeks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,6 +6,34 @@ class Navigation extends React.Component {
 
     componentDidMount(){
         
+        let btn = document.getElementsByClassName("dropbtn");
+        let dwn = document.getElementsByClassName("dropdown");
+
+        const handleClick = (e) => {
+            let flag = false;
+            // console.log(document.getElementById(e.target.id+"a").classList);
+            if(document.getElementById(e.target.id+"a").classList.contains("showdown"))
+                flag =true;
+            for(let i=0;i<btn.length;i++){
+                dwn[i].classList.remove("showdown");
+            }
+            // console.log(e.target.id + " " + flag);
+            if(flag == false)
+                document.getElementById(e.target.id+"a").classList.add("showdown");
+            
+        }
+
+        window.addEventListener("click",(e)=>{
+            for(let i=0;i<btn.length;i++)
+                if(btn[i].contains(e.target))
+                    return;
+            for(let i=0;i<dwn.length;i++)
+                dwn[i].classList.remove("showdown");
+        })
+
+        for(let i=0;i<btn.length;i++){
+            btn[i].addEventListener('click',handleClick);
+        }
         // Mobile Navigation
         $('.mobile-toggle').click(function() {
             if ($('.main_h').hasClass('open-nav')) {
@@ -15,30 +43,30 @@ class Navigation extends React.Component {
             }
         });
         
-        $('.main_h li a').click(function() {
-            if ($('.main_h').hasClass('open-nav')) {
-                $('.navigation').removeClass('open-nav');
-                $('.main_h').removeClass('open-nav');
-            }
-        });
+        // $('.main_h li a').click(function() {
+        //     if ($('.main_h').hasClass('open-nav')) {
+        //         $('.navigation').removeClass('open-nav');
+        //         $('.main_h').removeClass('open-nav');
+        //     }
+        // });
         
         // navigation scroll lijepo radi materem
-        $('nav a').click(function(event) {
-            var id = $(this).attr("href");
-            var offset = 70;
-            var target = $(id).offset().top - offset;
-            $('html, body').animate({
-                scrollTop: target
-            }, 500);
-            event.preventDefault();
-        });
+        // $('nav a').click(function(event) {
+        //     var id = $(this).attr("href");
+        //     var offset = 70;
+        //     var target = $(id).offset().top - offset;
+        //     $('html, body').animate({
+        //         scrollTop: target
+        //     }, 500);
+        //     event.preventDefault();
+        // });
     }
     
 
     render() {
         return (
             <div>
-                <header class="main_h green">
+                <header class="main_h indigo">
                     <div class="row">
                         <a class="logo" href="#"><img src="./images/logo.png"></img></a>
                         <div class="mobile-toggle">
@@ -48,10 +76,42 @@ class Navigation extends React.Component {
                         </div>
                         <nav>
                             <ul>
-                                <li className="highlightTextOut"><a alt="Section 01" href=".sec01">Section 01</a></li>
-                                <li className="highlightTextOut"><a href=".sec02">Section 02</a></li>
-                                <li className="highlightTextOut"><a href=".sec03">Section 03</a></li>
-                                <li className="highlightTextOut"><a href=".sec04">Section 04</a></li>
+                                <li className="highlightTextOut">
+                                    <div className="dropbtn">
+                                        <a alt="Section" id="1">Section</a>
+                                    </div>
+                                    <div className="dropdown" id="1a">
+                                        <a alt="Hey">hey</a>
+                                        <a alt="there">there</a>
+                                    </div>
+                                </li>
+                                <li className="highlightTextOut">
+                                    <div className="dropbtn">
+                                        <a alt="Section" id="2">Section</a>
+                                    </div>
+                                    <div className="dropdown" id="2a">
+                                        <a alt="Hey">hey</a>
+                                        <a alt="there">there</a>
+                                    </div>
+                                </li>
+                                <li className="highlightTextOut">
+                                    <div className="dropbtn">
+                                        <a alt="Section" id="3">Section</a>
+                                    </div>
+                                    <div className="dropdown" id="3a">
+                                        <a alt="Hey">hey</a>
+                                        <a alt="there">there</a>
+                                    </div>
+                                </li>
+                                <li className="highlightTextOut">
+                                    <div className="dropbtn">
+                                        <a alt="Section" id="4">Section</a>
+                                    </div>
+                                    <div className="dropdown" id="4a">
+                                        <a alt="Hey">hey</a>
+                                        <a alt="there">there</a>
+                                    </div>
+                                </li>
                             </ul>
                         </nav>
 
